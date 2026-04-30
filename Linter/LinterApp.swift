3fe196ea9492db6ca0ae78b9188e4262bbdefda9
Alex Bilozor <bilozor.dev@gmail.com>
2026-04-30 10:17:47 -0700

    Initial Commit

---
+++ Linter/LinterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinterApp.swift
+//  Linter
+//
+//  Created by Alex Bilozor on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct LinterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
